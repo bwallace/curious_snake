8dc63d2f41234891cfda91fba153ce440d1b8439
byron wallace <byronwallace@byron-wallaces-macbook-pro.local>
2009-06-07 17:57:10 -0400

    cleaned up antiquated references

--- dataset.py
+++ dataset.py
@@ -1,14 +1,10 @@
 '''
-###############################################
-
     dataset.py
     Byron C Wallace
     Tufts Medical Center
         
     This module contains methods and classes for parsing
     and manipulating datasets.
-
-###############################################
 '''
 
 import pdb
@@ -40,17 +36,18 @@ def line_to_instance(l, id):
     except:
         pdb.set_trace()
     # remove new line characters; these were causing headaches in certain cases
-    point = l_split[1:] #[x for x in l_split[1:] if x != "\n"]
+    point = l_split[1:]
     dict_point = {}
     for coord, value in [dimension.split(":") for dimension in point if not point[0]=='']:
       try:
           dict_point[eval(coord)] = eval(value)
       except:
           pdb.set_trace()
-    
+
     return instance(id, dict_point, label)
 
-  
+
+        
 class instance:
     '''
     Represents a single point/label combination. The label doesn't necessarily
@@ -59,21 +56,17 @@ class instance:
     '''
     point = None
     label = None
-    real_label = None
     id = None
     name = None
-    has_synthetic_label = None
+
   
     def __init__(self, id, point, label=None, name="", is_synthetic=False):
         self.id = id
-        self.real_label = label
         self.point = point
         self.label = label
         self.name = name
-        self.has_synthetic_label = is_synthetic
         
     def set_synthetic_label(self, synth_lbl):
-        self.has_synthetic_label = True
         self.label = synth_lbl
 
 class dataset:
@@ -86,9 +79,15 @@ class dataset:
     name = None
     
     def __init__(self, instances, name=""):
-      self.instances = instances
-      self.name = None
+        self.instances = instances
+        self.name = None
     
+    def size(self):
+        if self.instances is not None:
+            return len(self.instances)
+        else:
+            return None
+        
     def remove_instances(self, ids_to_remove):
         '''
         Remove and return the instances with ids found in the
@@ -102,6 +101,7 @@ class dataset:
                 self.instances.remove(instance)
                 break
         return removed_instances
+      
        
     def copy(self):
          return dataset(list(self.instances)) 
@@ -133,27 +133,13 @@ class dataset:
         self.assert_unique_instances()
         
     def assert_unique_instances(self):
-        # does not include synthetics!
         ids = self.get_instance_ids()
         if not len(ids) == len(set(ids)):
             pdb.set_trace()
             raise Exception, "duplicate instance ids!"
-
-    def number_of_false_minorities(self):
-        fake_pos_count = 0
-        for inst in self.instances:
-            if inst.real_label != self.minority_class and inst.label == self.minority_class:
-                fake_pos_count += 1
-        return fake_pos_count
-       
-    def get_synthetic_ids(self):
-        return [inst.id for inst in self.instances if inst.has_synthetic_label]
+            
          
     def pick_random_minority_instances(self, k):
-        '''
-        Returns a list of randomly selected minority instance ids
-        (NOTE: this uses the labels! Obviously this is cheating)
-        '''
         min_ids = self.get_list_of_minority_ids()
 
         if not len(min_ids) >= k:
@@ -167,11 +153,8 @@ class dataset:
         
         return selected_ids
         
+        
     def pick_random_majority_instances(self, k):
-        '''
-        Returns a list of randomly selected majority instance ids
-        (NOTE: this uses the labels! Obviously this is cheating)
-        '''
         maj_ids = self.get_list_of_majority_ids()
  
         if not len(maj_ids) >= k:
@@ -188,14 +171,14 @@ class dataset:
     def get_list_of_minority_ids(self, include_false_minorities=False):
         minorities = []
         for inst in self.instances:
-            inst_lbl = inst.real_label
+            inst_lbl = inst.label
             if include_false_minorities:
                 inst_lbl = inst.label
             if inst_lbl == self.minority_class:
                 minorities.append(inst.id)
         return minorities
         
-    def get_minority_examples(self, include_synthetics=False):
+    def get_minority_examples(self):
         minorities = []
         for inst in self.instances:
             if inst.label == self.minority_class:
@@ -204,45 +187,32 @@ class dataset:
         
     def get_points_str(self):
         out_s = []
-        #pdb.set_trace()
         for inst in self.instances:
             inst_str = []
             inst_str.append(str(inst.label))
             for v in inst.point.values():
                 inst_str.append(str(v))
             out_s.append(",".join(inst_str))
-        #pdb.set_trace()
         return "\n".join(out_s)
+        
         
     def get_list_of_majority_ids(self, majority_id=-1):
         majorities = []
         for inst in self.instances:
-            inst_lbl = inst.real_label
+            inst_lbl = inst.label
             if inst_lbl == majority_id:
                 majorities.append(inst.id)
         return majorities
         
-        
-    def number_of_minority_examples(self,  include_synthetics=True, use_real_label=False):
+    def number_of_minority_examples(self):
         '''
         Counts and returns the number of minority examples in this dataset.
-        
-        The include_synthetics flag means when we count the + examples we do not exclude
-        synthetics, or fakes. 
         '''
-        if use_real_label:
-            if not include_synthetics:
-                return len([inst for inst in self.instances if inst.real_label == self.minority_class and not inst.has_synthetic_label])
-            else:
-                return len([inst for inst in self.instances if inst.real_label == self.minority_class])
-                
-        if not include_synthetics:
-            return len([inst for inst in self.instances if inst.label == self.minority_class and not inst.has_synthetic_label])
-        else:
-            return len([inst for inst in self.instances if inst.label == self.minority_class])
-
+        return len([inst for inst in self.instances if inst.label == self.minority_class])
+    
+    
     def get_instance_ids(self):
-        return [inst.id for inst in self.instances if not inst.has_synthetic_label]
+        return [inst.id for inst in self.instances]
     
     def get_majority_examples(self):
         majorities = []
@@ -250,12 +220,24 @@ class dataset:
             if inst.label != self.minority_class:
                 majorities.append(inst)
         return dataset(majorities)
-        
-    def number_of_majority_examples(self, include_synthetics=False):
+
+    def number_of_majority_examples(self):
         '''
         Counts and returns the number of majority examples in this dataset.
         '''
-        return len(self.instances) - self.number_of_minority_examples(include_synthetics=include_synthetics)
+        return len(self.instances) - self.number_of_minority_examples()
+    
+    
+    def get_examples_with_label(self, label):
+        '''
+        Returns a new dataset with all the examples that have the parametric label.
+        '''
+        examples = []
+        for inst in self.instances:
+            if inst.label == label:
+                examples.append(inst)
+        return dataset(examples)
+        
     
     def get_and_remove_random_subset(self, n):
         '''
@@ -283,9 +265,9 @@ class dataset:
         return [inst.point for inst in self.instances]
 
     def get_labels(self):
-        return [inst.real_label for inst in self.instances]
+        return [inst.label for inst in self.instances]
 
-    def get_samples_and_labels(self, include_synthetics=False):
+    def get_samples_and_labels(self):
         '''
         Returns a tuple of [[s_1, s_2, ..., s_n], [l_1, l_2, ..., l_n]] where s_i is the ith feature 
         vector and l_i is its label.
@@ -295,9 +277,8 @@ class dataset:
         samples = []
         labels = []
         for inst in self.instances:
-            if not inst.has_synthetic_label or include_synthetics:
-                samples.append(inst.point)
-                labels.append(inst.label)   
+            samples.append(inst.point)
+            labels.append(inst.label)   
         return [samples, labels]
     
   
